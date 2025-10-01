44607cd0aac37294bdcb59cf7e3f94f88dedf50c
default <nicholas.sun@stu.ocsb.ca>
2025-10-01 22:45:29 +0000

    Added string literal programs

---
+++ main.go
@@ -0,0 +1,22 @@
+/**
+ * @author Nicholas Sun
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview This program practices string literals.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// String literals
+	fmt.Println("I love Computer Science!!")
+
+	fmt.Println("I    love  Computer Science!")
+
+	fmt.Println("I   love   Computer   Science   100%")
+
+	fmt.Println("\nDone.")
+}
+
---
+++ main.ts
@@ -0,0 +1,16 @@
+/**
+ * @author Nicholas Sun
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview This program practices string literals.
+ */
+
+// String literals
+console.log("I love Computer Science!!");
+
+console.log("I    love  Computer Science!");
+
+console.log("I   love   Computer   Science   100%");
+
+console.log("\nDone.");
+
